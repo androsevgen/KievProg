07bc86dbaae1460d09adf935a5e1c1474a34d563
andros.evgen <andros.evgen@gmail.com>
2016-10-03 16:37:23 +0300

    commit

---
+++ src/L1_1/palindromes.java
@@ -0,0 +1,30 @@
+package L1_1;
+
+import java.util.Scanner;
+
+/**
+ * Created by admin on 03.10.2016.
+ */
+public class palindromes {
+    public static void main(String[] args) {
+
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Ввести число: ");
+
+            for (int i = 0; i < 5; i++) {
+
+                String o, reverse = "";
+                o = scanner.nextLine();
+                int leg = o.length();
+                for (int j = leg - 1; j >= 0; j--) {
+                    reverse = reverse + o.charAt(j);
+                    if (o.equals(reverse)) System.out.println(o);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
